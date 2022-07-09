0dde1c9f42383a50d2304dad361db28199d9353a
Mitch <mitch.shiotani1@gmail.com>
2022-07-09 10:20:53 +0100

    fixes on pi for displayal of time info after actually running the code

--- src/renderers/timekeeper.py
+++ src/renderers/timekeeper.py
@@ -10,6 +10,7 @@ class TimekeeperRenderer:
     self.game = self.data.current_game()
     # self.default_colors = self.data.config.team_colors.color("default")
     self.default_colors = {'r': 255, 'g': 255, 'b': 255} # just setting to white for now, used for text
+    self.color_graphics = graphics.Color(self.default_colors['r'], self.default_colors['g'], self.default_colors['b'])
 
     ### TODO: going to change data here for testing purposes ###
     self.game.time_and_period = '1:23 - 4th'
@@ -19,25 +20,19 @@ class TimekeeperRenderer:
 
   def render(self):
     self.__render_game_time_text()
-    self.__render_downs_text()
-    # self.__render_field_position_text() # TODO: uncomment later
+    self.__render_downs_and_field_position_text()
 
   def __render_game_time_text(self):
     # get coords from config
     coords = self.data.config.layout.coords("time")
     font = self.data.config.layout.font("teams.scores.home") # TODO:honestly have no idea how the fonts are being pulled. should figure it out.
     # get data from data object
-    graphics.DrawText(self.canvas, font['font'], coords['x'], coords['y'], self.default_colors, self.game.time_and_period)
+    graphics.DrawText(self.canvas, font['font'], coords['x'], coords['y'], self.color_graphics, self.game.time_and_period)
     return 1
 
-  def __render_downs_text(self):
+  def __render_downs_and_field_position_text(self):
     coords = self.data.config.layout.coords("downs")
     font = self.data.config.layout.font("teams.scores.home") # TODO:honestly have no idea how the fonts are being pulled. should figure it out.
-    graphics.DrawText(self.canvas, font['font'], coords['x'], coords['y'], self.default_colors, self.game.down_and_distance)
-    return 1
-
-  def __render_field_position_text(self):
-    coords = self.data.config.layout.coords("field_position")
-    font = self.data.config.layout.font("teams.scores.home") # TODO:honestly have no idea how the fonts are being pulled. should figure it out.
-    graphics.DrawText(self.canvas, font['font'], coords['x'], coords['y'], self.default_colors, self.game.field_position)
+    text = "{}, {}".format(self.game.down_and_distance, self.game.field_position)
+    graphics.DrawText(self.canvas, font['font'], coords['x'], coords['y'], self.color_graphics, text)
     return 1
