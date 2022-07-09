263b000314bc658e9d4ef6e0e0dcc593b82510d8
mitch <mitch.shiotani1@gmail.com>
2022-07-09 15:23:06 +0900

    added line to install pipenv (at least I think I need this. should check)

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ git checkout main
 git fetch origin --prune
 git pull
 sudo apt-get install libxml2-dev libxslt-dev
+sudo pip install --user pipenv
 sudo pipenv install --system # TODO: need to install pipenv beforehand, probably put that somewhere in this script
 make
 echo "If you didn't see any errors above, everything should be installed!"
