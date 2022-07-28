041eab0454f3ff8ce1654fe4f164214a8094b3a8
mickshio <mickey.shiotani1@gmail.com>
2022-07-28 11:03:05 +0900

    added time difference functionality

--- src/renderers/pregameinfo.py
+++ src/renderers/pregameinfo.py
@@ -1,5 +1,6 @@
+from time import time
 from rgbmatrix import graphics
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class PregameInfoRenderer:
   def __init__(self, canvas, data):
@@ -15,7 +16,9 @@ class PregameInfoRenderer:
     self.__render_broadcast_info()
 
   def __render_gametime(self):
-    start_time_text = datetime.strptime(self.game.start_time, '%Y-%m-%dT%H:%MZ').strftime("%m/%d %H:%M");
+    start_time_changed = datetime.strptime(self.game.start_time, '%Y-%m-%dT%H:%MZ') + timedelta(hours=self.data.config.time_difference.utc_difference)
+    start_time_text = start_time_changed.strftime("%m/%d %H:%M")
+    # start_time_text = datetime.strptime(self.game.start_time, '%Y-%m-%dT%H:%MZ').strftime("%m/%d %H:%M")
     # TODO: need to make this show in local time, but I'll have to figure out how to do that
     coords = self.data.config.layout.coords("pre_game_info.gametime")
     font = self.data.config.layout.font("teams.scores.home") # TODO:honestly have no idea how the fonts are being pulled. should figure it out.
