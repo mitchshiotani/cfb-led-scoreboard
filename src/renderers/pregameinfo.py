8b6ab117b30ff6256f65ddc7d9a977f9bfed51db
mitch <mitch.shiotani1@gmail.com>
2022-08-11 14:22:29 +0900

    removed unnecessary comments

--- src/renderers/pregameinfo.py
+++ src/renderers/pregameinfo.py
@@ -18,8 +18,6 @@ class PregameInfoRenderer:
   def __render_gametime(self):
     start_time_changed = datetime.strptime(self.game.start_time, '%Y-%m-%dT%H:%MZ') + timedelta(hours=self.data.config.utc_difference)
     start_time_text = start_time_changed.strftime("%m/%d %H:%M")
-    # start_time_text = datetime.strptime(self.game.start_time, '%Y-%m-%dT%H:%MZ').strftime("%m/%d %H:%M")
-    # TODO: need to make this show in local time, but I'll have to figure out how to do that
     coords = self.data.config.layout.coords("pre_game_info.gametime")
     font = self.data.config.layout.font("teams.scores.home") # TODO:honestly have no idea how the fonts are being pulled. should figure it out.
     graphics.DrawText(self.canvas, font['font'], coords['x'], coords['y'], self.color_graphics, start_time_text)
